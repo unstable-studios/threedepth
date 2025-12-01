a4105c97eb77e9ddef6eabc931ff4ef1ddfc10a9
Mike Hansen <mike@mkhnsn.com>
2025-11-30 21:10:50 -0800

    remove old prototype from routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router';
 import Layout from './Layout';
-
 import Editor from './Editor';
-import { StlToDepthMap } from './StlToDepthMap';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
@@ -12,10 +10,6 @@ createRoot(document.getElementById('root')!).render(
 			<Routes>
 				<Route element={<Layout />}>
 					<Route index element={<Editor />} />
-					<Route
-						path='/v1'
-						element={<StlToDepthMap width={1024} height={1024} />}
-					/>
 				</Route>
 			</Routes>
 		</BrowserRouter>
